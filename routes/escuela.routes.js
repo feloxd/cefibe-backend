f247201407d29d94324bedc7bd3ec4a1cf9b2d30
feloxd <feloxd6478@gmail.com>
2026-01-18 17:35:31 -0600

    Add files via upload

--- routes/escuela.routes.js
+++ routes/escuela.routes.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const router = express.Router();
-
+const upload = require('../ftpUploader.js'); // Importamos el middleware que ya usas para alumnos
 
 const {
     getAllEscuelas,
     createEscuela,
-    updateEscuela,  
-    deleteEscuela   
+    updateEscuela,
+    deleteEscuela
 } = require('../controllers/escuela.controller.js');
 
-
 router.get('/', getAllEscuelas);
-router.post('/', createEscuela);
 
+// Agregamos el middleware .single('logo') para capturar el archivo
+// El nombre 'logo' debe coincidir con el campo que enviemos desde el frontend
+router.post('/', upload.single('logo'), createEscuela);
 
-router.put('/:id', updateEscuela);    
-router.delete('/:id', deleteEscuela); 
+router.put('/:id', updateEscuela);
+router.delete('/:id', deleteEscuela);
 
 module.exports = router;
