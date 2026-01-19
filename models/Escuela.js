49b8759c072bc2c22a1dd8e67e7c87f612ab7641
feloxd <feloxd6478@gmail.com>
2026-01-18 18:38:01 -0600

    Add files via upload

--- models/Escuela.js
+++ models/Escuela.js
@@ -2,18 +2,18 @@ const { DataTypes } = require('sequelize');
 const { sequelize } = require('../config/db');
 
 const Escuela = sequelize.define('Escuela', {
-    
+
     nombre: {
         type: DataTypes.STRING,
-        allowNull: false, 
-        unique: true 
+        allowNull: false,
+        unique: true
     },
     contacto: {
-        type: DataTypes.STRING, 
+        type: DataTypes.STRING,
         allowNull: true,
     },
     status: {
-        type: DataTypes.STRING, 
+        type: DataTypes.STRING,
         allowNull: true,
     },
     ciudad: {
@@ -21,6 +21,11 @@ const Escuela = sequelize.define('Escuela', {
         allowNull: true,
     },
     pais: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    // NUEVA COLUMNA: Sin esto, Sequelize nunca mostrará el logo en la web
+    logo_url: {
         type: DataTypes.STRING,
         allowNull: true,
     }
