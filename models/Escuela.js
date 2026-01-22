cb1355a6986d66c4be4b0179557b1cea0eb3c8e6
feloxd <feloxd6478@gmail.com>
2026-01-21 21:44:21 -0600

    Add files via upload
    escuela js

--- models/Escuela.js
+++ models/Escuela.js
@@ -2,11 +2,15 @@ const { DataTypes } = require('sequelize');
 const { sequelize } = require('../config/db');
 
 const Escuela = sequelize.define('Escuela', {
-
     nombre: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true
+    },
+    // NUEVA COLUMNA: Para que el buscador de Fer funcione por numero oficial
+    matricula: {
+        type: DataTypes.STRING(50),
+        allowNull: true, // Lo ponemos true por si hay escuelas viejas sin matricula, pero en el admin sera obligatorio
     },
     contacto: {
         type: DataTypes.STRING,
@@ -24,11 +28,14 @@ const Escuela = sequelize.define('Escuela', {
         type: DataTypes.STRING,
         allowNull: true,
     },
-    // NUEVA COLUMNA: Sin esto, Sequelize nunca mostrará el logo en la web
     logo_url: {
         type: DataTypes.STRING,
         allowNull: true,
     }
+}, {
+    // Esto asegura que Sequelize use exactamente el nombre de la tabla que vimos en phpMyAdmin
+    tableName: 'Escuelas',
+    timestamps: false
 });
 
 module.exports = Escuela;
